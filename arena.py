c42c9e562d489ec6716ab6ac5eea9f5687c69711
Wochozka <david@svarc.it>
2023-04-28 13:06:05 +0200

    arena

--- arena.py
+++ arena.py
@@ -11,11 +11,44 @@ class Arena():
         self._kostka = kostka
     
     def zapas(self):
+        
         while self.__bojovnik1.nazivu and self.__bojovnik2.nazivu:
             self.__bojovnik1.utok(self.__bojovnik2)
-            print(self.__bojovnik1.getZprava())
-            self.__bojovnik2.utok(self.__bojovnik1)
-            print(self.__bojovnik2.getZprava())
+            self.vykresli()
+            self.vypis_zpravu(self.__bojovnik1.getZprava())
+            self.vypis_zpravu(self.__bojovnik2.getZprava())
+            if self.__bojovnik2.nazivu:
+                self.__bojovnik2.utok(self.__bojovnik1)
+                self.vykresli()
+                self.vypis_zpravu(self.__bojovnik2.getZprava())
+                self.vypis_zpravu(self.__bojovnik1.getZprava())
+
+    
+    def vycisti_obrazovku(self):
+        import sys as _sys
+        import subprocess as _subprocess
+
+        if _sys.platform.startswith("win"):
+            _subprocess.call(["cmd.exe", "/C", "cls"])
+        else:
+            _subprocess.call("clear")
+
+    def vypis_zpravu(self, zprava):
+        import time as _time
+        print(zprava)
+        _time.sleep(1)
+
+    def vykresli_bojovnika(self, bojovnik):
+        print(bojovnik)
+        print(f"Zivot: {bojovnik.graficky_zivot()}")
+    
+    def vykresli(self):
+        self.vycisti_obrazovku()
+        print("---------------- Arena --------------------\n")
+        self.vykresli_bojovnika(self.__bojovnik1)
+        print()
+        self.vykresli_bojovnika(self.__bojovnik2)
+
 
 if __name__=="__main__":
     k1 = kostka.Kostka()
